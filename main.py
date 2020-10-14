2651f9bd190964608124eeae02b33260f9d9312c
Tim <tim.groth1997@hotmail.de>
2020-10-14 20:26:36 +0200

    get source and target language/new example file

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ root = tree.getroot()
 
 #Find all trans-units with target state=needs-translation
 for file in root.iter():
-    if file.tag == '{urn:oasis:names:tc:xliff:document:1.2}group' :
+    if file.tag == '{urn:oasis:names:tc:xliff:document:1.2}file':
+        source_language = file.get('source-language')
+        target_language = file.get('target-language')
+    elif file.tag == '{urn:oasis:names:tc:xliff:document:1.2}group' :
         if file.get('id'):
             for trans_unit in file.findall('{urn:oasis:names:tc:xliff:document:1.2}trans-unit'):
                 target = trans_unit.find('{urn:oasis:names:tc:xliff:document:1.2}target')
                 try:
                     if target.get('state') == 'needs-translation':
+                        #source nur zu testzwecken gesucht
                         source = trans_unit.find('{urn:oasis:names:tc:xliff:document:1.2}source')
                         if source is not None:
                             print(source.text) #//TODO: Suche und Ersetzung ion Zieldatei einbauen
