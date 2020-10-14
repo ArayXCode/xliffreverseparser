dcab852b13cfb864e2b50a6f8762a22e9441f388
Tim Groth <tim.groth@cosmoconsult.com>
2020-10-14 15:45:34 +0200

    clean search for targets with translation needs

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import sys
 import os
-from lxml import etree
+import fs
+
 from xml.etree import ElementTree as ET
 
+
+#Create tree from .xlf File //TODO: dateinput variabel gestalten
 tree = ET.parse('country_data.xlf')
 root = tree.getroot()
 
-#finde start der gruppierung
+#Find all trans-units with target state=needs-translation
 for file in root.iter():
     if file.tag == '{urn:oasis:names:tc:xliff:document:1.2}group' :
         if file.get('id'):
-            group_start = file
-
-#iteration über alle trans_units, findet den translation state, zurzeit wird er nur gedruckt
-try:
-    for trans_unit in group_start.findall('{urn:oasis:names:tc:xliff:document:1.2}trans-unit'):
-        target = trans_unit.find('{urn:oasis:names:tc:xliff:document:1.2}target')
-        if target is not None:
-            print(target.get('state'))
-except:
-    print("Something went wrong!")
+            for trans_unit in file.findall('{urn:oasis:names:tc:xliff:document:1.2}trans-unit'):
+                target = trans_unit.find('{urn:oasis:names:tc:xliff:document:1.2}target')
+                try:
+                    if target.get('state') == 'needs-translation':
+                        source = trans_unit.find('{urn:oasis:names:tc:xliff:document:1.2}source')
+                        if source is not None:
+                            print(source.text) #//TODO: Suche und Ersetzung ion Zieldatei einbauen
+                except:
+                    print("no target found!")
