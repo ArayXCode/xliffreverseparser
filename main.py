0afac72e22ebca43a20a0f12f7421a025cb619c3
Tim Groth <tim.groth@cosmoconsult.com>
2020-10-15 13:43:11 +0200

    added function wrapper/ added find file

--- main.py
+++ main.py
@@ -4,25 +4,58 @@ import fs
 
 from xml.etree import ElementTree as ET
 
+#init global variables
+xliff_translated_result = []
+source_language, target_language = '',''
 
-#Create tree from .xlf File //TODO: dateinput variabel gestalten
-tree = ET.parse('country_data.xlf')
-root = tree.getroot()
+#Find all trans-units with target state=needs-translation from input .xlf file
+def findTransFromSource(filename: str):
+    #Create tree from .xlf File //TODO: dateinput variabel gestalten
+    if filename != '':
+        tree = ET.parse(filename)
+        root = tree.getroot()
 
-#Find all trans-units with target state=needs-translation
-for file in root.iter():
-    if file.tag == '{urn:oasis:names:tc:xliff:document:1.2}file':
-        source_language = file.get('source-language')
-        target_language = file.get('target-language')
-    elif file.tag == '{urn:oasis:names:tc:xliff:document:1.2}group' :
-        if file.get('id'):
-            for trans_unit in file.findall('{urn:oasis:names:tc:xliff:document:1.2}trans-unit'):
-                target = trans_unit.find('{urn:oasis:names:tc:xliff:document:1.2}target')
-                try:
-                    if target.get('state') == 'needs-translation':
-                        #source nur zu testzwecken gesucht
-                        source = trans_unit.find('{urn:oasis:names:tc:xliff:document:1.2}source')
-                        if source is not None:
-                            print(source.text) #//TODO: Suche und Ersetzung ion Zieldatei einbauen
-                except:
-                    print("no target found!")
+    
+    for file in root.iter():
+        if file.tag == '{urn:oasis:names:tc:xliff:document:1.2}file':
+            source_language = file.get('source-language')
+            target_language = file.get('target-language')
+        elif file.tag == '{urn:oasis:names:tc:xliff:document:1.2}group' :
+            #group ebene
+            if file.get('id') == 'body':
+                #trans-unit ebene
+                for trans_unit in file.findall('{urn:oasis:names:tc:xliff:document:1.2}trans-unit'):
+                    target = trans_unit.find('{urn:oasis:names:tc:xliff:document:1.2}target')
+                    if target is not None:
+                        #Wenn target state= translated, sichere übersetzung
+                        dev_translation, xliff_generated_reference = '',''
+                        if target.get('state') == 'translated':
+                            for note in trans_unit.findall('{urn:oasis:names:tc:xliff:document:1.2}note'):
+                                if note.get('from') == 'Developer':
+                                    dev_translation = note.text
+                                elif note.get('from') == 'Xliff Generator':
+                                    xliff_generated_reference = note.text
+                        if (dev_translation != '') and (xliff_generated_reference != ''):
+                            if (target_language != '') and (source_language != ''):
+                                if (source_language in dev_translation) or (target_language in dev_translation):
+                                    xliff_translated_result.append([xliff_generated_reference, dev_translation])
+    #return result alphabetically sorted
+    return(sorted(xliff_translated_result))
+
+#find file name for translation results
+def findFileForTransResult(list_to_sort: list):
+    sorted_trans_list = []
+    for element in list_to_sort:
+        file_to_extend = element[0].split(' -')[0]
+        rest_of_file_to_extend = element[0].split('- ')[1] + ' ' + element[1]    
+        sorted_trans_list.append([file_to_extend, rest_of_file_to_extend])
+    return(sorted_trans_list)
+
+
+
+
+
+#call defined funcitons
+kekw = findFileForTransResult(findTransFromSource('country_data.xlf'))
+
+print(kekw)
